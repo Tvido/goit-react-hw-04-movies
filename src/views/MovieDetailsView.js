05cd93576a50e5bc592741d410931a5cd911b8e7
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-28 17:32:17 +0300

    Create MovieDetailsView.js

---
+++ src/views/MovieDetailsView.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
+import axios from 'axios';
+
+class MovieDetailsView extends Component {
+  state = {
+    movie: null,
+    genres: [],
+    id: null,
+    title: null,
+    poster_path: null,
+    overview: null,
+    popularity: null,
+    release_date: null,
+  };
+
+  async componentDidMount() {
+    const API_KEY = '822df93417c2789fa5aa8e4d6553456b';
+    const { movieId } = this.props.match.params;
+
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`,
+    );
+
+    // this.setState({ movie: response.data });
+    this.setState({ ...response.data });
+  }
+  render() {
+    const {
+      // movie,
+      genres,
+      id,
+      title,
+      poster_path,
+      overview,
+      popularity,
+      release_date,
+    } = this.state;
+
+    const { match } = this.props;
+
+    const date = `${release_date}`.substr(0, 4);
+
+    return (
+      <>
+        <div key={id} className="">
+          <h1>MOVIE DETAILS {this.props.match.params.movieId}</h1>
+
+          <div className="">
+            {poster_path ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w300${poster_path}`}
+                alt={title}
+                className=""
+              />
+            ) : (
+              <p className="">No photo</p>
+            )}
+          </div>
+
+          <div className="MoviePage-block">
+            <h1>
+              {title}({date})
+            </h1>
+            <p>User Score: {Math.round(popularity)}%</p>
+            <h2>Overview</h2>
+            <p>{overview}</p>
+            <h3>Genres</h3>
+            <ul>
+              {genres.map(genre => (
+                <li key={genre.id}>{genre.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="MoviePage-menu">
+          <h2>Additional information</h2>
+
+          <ul>
+            <li>
+              <NavLink
+                to={`${match.url}/cast`}
+                className="NavLink"
+                activeClassName="NavLink-activ"
+              >
+                Cast
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`${match.url}/reviews`}
+                className="NavLink"
+                activeClassName="NavLink-activ"
+              >
+                Reviews
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
+
+export default MovieDetailsView;
