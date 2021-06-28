9f9d084049405136a3fc204a62d98a770f1c74fe
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-28 15:22:16 +0300

    Create MoviesView.js

---
+++ src/views/MoviesView.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import axios from 'axios';
+
+class MoviesView extends Component {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    const API_KEY = '822df93417c2789fa5aa8e4d6553456b';
+    // const trendingPeriod = 'day';
+    const trendingPeriod = 'week';
+
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/${trendingPeriod}?api_key=${API_KEY}`,
+    );
+
+    this.setState({ movies: response.data.results });
+  }
+
+  render() {
+    return (
+      <>
+        <ul>
+          <h1>THIS WEEK TOP RATED</h1>
+
+          {this.state.movies.map(movie => (
+            // <li key={movie.id}>{movie.title}</li>
+            <li key={movie.id}>
+              <NavLink to={`/movies/${movie.id}`} className="MovieLink">
+                {movie.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default MoviesView;
