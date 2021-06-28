e9491d7bbe7400a5a6115e227211864c17a0bc77
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-28 13:20:29 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
 import HomeView from './views/HomeView';
 import AuthorsView from './views/AuthorsView';
+import BooksView from './views/BooksView';
+import NotFoundView from './views/NotFoundView';
 
 import './App.css';
 
 const App = () => (
   <>
-    <Route exact path="/" component={HomeView} />
-    <Route path="/goit-react-hw-04-movies" component={AuthorsView} />
+    <ul>
+      <li>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/authors">Authors</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/books">BooksView</NavLink>
+      </li>
+    </ul>
+
+    <Switch>
+      <Route exact path="/" component={HomeView} />
+      <Route path="/authors" component={AuthorsView} />
+      <Route path="/books" component={BooksView} />
+      <Route component={NotFoundView} />
+    </Switch>
   </>
 );
 
---
+++ src/views/NotFoundView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundView = () => {
+  return <h1>NotFoundView</h1>;
+};
+
+export default NotFoundView;
