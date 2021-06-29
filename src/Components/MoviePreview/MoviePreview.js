4bb71cdfba4e64f5f2edaad453825c249bb90240
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-29 12:32:34 +0300

    Update MoviePreview.js

--- src/Components/MoviePreview/MoviePreview.js
+++ src/Components/MoviePreview/MoviePreview.js
@@ -1,36 +1,14 @@
 import React from 'react';
 
-const MoviePreview = ({
-  poster_path,
-  title,
-  date,
-  popularity,
-  overview,
-  genres,
-}) => (
-  <>
-    <div>
-      <img
-        src={`https://image.tmdb.org/t/p/w300${poster_path}`}
-        alt={title}
-        className=""
-      />
+const MoviePreview = ({ poster_path, title }) => (
+  <div className="">
+    <div className="">
+      <img src={poster_path} className="card-img-top" alt={title} />
     </div>
     <div className="">
-      <h1>
-        {title}({date})
-      </h1>
-      <p>User Score: {Math.round(popularity)}%</p>
-      <h2>Overview</h2>
-      <p>{overview}</p>
-      <h3>Genres</h3>
-      <ul>
-        {genres.map(genre => (
-          <li key={genre.id}>{genre.name}</li>
-        ))}
-      </ul>
+      <h5 className="">{title}</h5>
     </div>
-  </>
+  </div>
 );
 
 export default MoviePreview;
