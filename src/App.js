efecae76b257a9c4a19983eca120d8388980d267
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-29 12:58:01 +0300

    add Suspense lazy spliting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import routes from './routes';
 
-import HomeView from './views/HomeView';
-import MoviesView from './views/MoviesView';
-import MovieDetailsView from './views/MovieDetailsView';
 import AppBar from './Components/AppBar';
+// import HomeView from './views/HomeView';
+// import MoviesView from './views/MoviesView';
+// import MovieDetailsView from './views/MovieDetailsView';
 // import NotFoundView from './views/NotFoundView';
 
 import './App.css';
+
+const HomeView = lazy(() =>
+  import('./views/HomeView.js' /*webpackChunkName *home-view**/),
+);
+
+const MoviesView = lazy(() =>
+  import('./views/MoviesView.js' /*webpackChunkName *movies-view**/),
+);
+
+const MovieDetailsView = lazy(() =>
+  import(
+    './views/MovieDetailsView.js' /*webpackChunkName *movie-details-view**/
+  ),
+);
 
 const App = () => (
   <>
     <AppBar />
 
-    <Switch>
-      <Route exact path={routes.home} component={HomeView} />
-      <Route exact path={routes.moviesView} component={MoviesView} />
-      <Route path={routes.movieDetailsView} component={MovieDetailsView} />
-      {/* <Route component={NotFoundView} /> */}
-    </Switch>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Switch>
+        <Route exact path={routes.home} component={HomeView} />
+        <Route exact path={routes.moviesView} component={MoviesView} />
+        <Route path={routes.movieDetailsView} component={MovieDetailsView} />
+        {/* <Route component={NotFoundView} /> */}
+      </Switch>
+    </Suspense>
   </>
 );
 
