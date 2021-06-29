22b577f98d991f17240ec574552df2e0df2cb45e
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-29 12:32:07 +0300

    upd movieList

--- src/Components/MovieList/MovieList.js
+++ src/Components/MovieList/MovieList.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import MoviePreview from '../MoviePreview';
 
-const MovieList = ({ movies, match }) => {
+import '../../App.css';
+
+const MovieList = ({ movies, match, location }) => {
   return (
-    <ul>
-      {movies.map(movie => (
-        <li key={movie.id}>
-          <NavLink to={`${match.url}/${movie.id}`} className="">
-            {/* {movie.title} */}
-            <MoviePreview />
+    <ul className="MovieList">
+      {movies.map(({ id, poster_path, title }) => (
+        <li key={id}>
+          <NavLink
+            to={{ pathname: `${match.url}/${id}`, state: { from: location } }}
+            className=""
+          >
+            {/* <NavLink to={`${match.url}/${id}`} className=""> */}
+            {/* <img src={movie.poster_path} alt={title} className="" />
+            {movie.title} */}
+            <MoviePreview
+              poster_path={`https://image.tmdb.org/t/p/w300${poster_path}`}
+              title={title}
+            />
           </NavLink>
         </li>
       ))}
